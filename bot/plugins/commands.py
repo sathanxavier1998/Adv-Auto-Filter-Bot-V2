d791ed5ebf0014f133dfe33b7cc092daf7830e34
Rahul <58558341+rahulps1000@users.noreply.github.com>
2021-07-29 22:52:47 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -6,12 +6,14 @@ from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 from bot import Translation, LOGGER # pylint: disable=import-error
 from bot.database import Database # pylint: disable=import-error
+from bot.utils import is_subscribed
+
+subscribed = filters.create(is_subscribed)
 
 db = Database()
 
-@Client.on_message(filters.command(["start"]) & filters.private, group=1)
+@Client.on_message(filters.command(["start"]) & filters.private & subscribed, group=1)
 async def start(bot, update):
-    
     try:
         file_uid = update.command[1]
     except IndexError:
@@ -85,7 +87,37 @@ async def help(bot, update):
         parse_mode="html",
         reply_to_message_id=update.message_id
     )
-
+    
+@Client.on_message(filters.command(["start"]) & filters.private & ~ subscribed, group=1)
+async def nostart(bot, update):
+    try:
+        file_uid = update.command[1]
+    except IndexError:
+        file_uid = False
+    if file_id:
+        buttons = [[
+            InlineKeyboardButton('Refresh 🔃', callback_data=f'refresh_btn|{file_id}')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text='You should join',
+            reply_markup=reply_markup,
+            parse_mode="html",
+            reply_to_message_id=update.message_id
+        )
+    else:
+        buttons = [[
+            InlineKeyboardButton('Refresh 🔃', callback_data='refresh_btn|None')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text='You should join',
+            reply_markup=reply_markup,
+            parse_mode="html",
+            reply_to_message_id=update.message_id
+        )
 
 @Client.on_message(filters.command(["about"]) & filters.private, group=1)
 async def about(bot, update):
