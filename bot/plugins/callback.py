0910f5dc1d4eb9e6ff3c30f8e54afc8e9bf28847
Rahul <58558341+rahulps1000@users.noreply.github.com>
2021-07-29 23:01:01 +0530

    Update callback.py

--- bot/plugins/callback.py
+++ bot/plugins/callback.py
@@ -18,8 +18,64 @@ from bot.plugins.settings import( # pylint: disable=import-error
     remove_emoji
 )
 from bot.database import Database # pylint: disable=import-error
+from bot.utils import is_subscribed
+
+subscribed = filters.create(is_subscribed)
 
 db = Database()
+
+@Client.on_callback_query(filters.regex(r"^refresh_btn|[\w*]") & subscribed, group=2)
+async def btnreferesh(bot, update: CallbackQuery):
+    data = update.data
+    file_uid = data.split('|',1)[1]
+    if file_uid != 'None':
+        file_id, file_name, file_caption, file_type = await db.get_file(file_uid)
+        
+        if (file_id or file_type) == None:
+            return
+        
+        caption = file_caption if file_caption != ("" or None) else ("<code>" + file_name + "</code>")
+        try:
+            await update.reply_cached_media(
+                file_id,
+                quote=True,
+                caption = caption,
+                parse_mode="html",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton
+                                (
+                                    '💢Join Our Channel💢', url="https://t.me/Cinemaathattakam_Links"
+                                )
+                        ]
+                    ]
+                )
+            )
+        except Exception as e:
+            await update.reply_text(f"<b>Error:</b>\n<code>{e}</code>", True, parse_mode="html")
+            LOGGER(__name__).error(e)
+        return
+
+    buttons = [[
+        InlineKeyboardButton('💥Developer💥', url='https://t.me/sathan_of_telegram'),
+        InlineKeyboardButton('🔰Our Group 🔰', url ='https://t.me/cinemaathattakam_group')
+    ],[
+        InlineKeyboardButton('Support 🛠', url='https://t.me/Cinemaathattakam_links')
+    ],[
+        InlineKeyboardButton('Help ⚙', callback_data="help")
+    ]]
+    
+    reply_markup = InlineKeyboardMarkup(buttons)
+    
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.START_TEXT.format(
+                update.from_user.first_name),
+        reply_markup=reply_markup,
+        parse_mode="html",
+        reply_to_message_id=update.message_id
+    )
 
 
 @Client.on_callback_query(filters.regex(r"navigate\((.+)\)"), group=2)
